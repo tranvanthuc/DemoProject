adf0097de88f1c3b993a32f2b370b6698d05c2fc
Tran Van Thuc <tranvanthucduytan@gmail.com>
2020-09-05 17:31:10 +0700

    Routing and Controller

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -6,8 +6,8 @@ use Illuminate\Http\Request;
 
 class TestController extends Controller
 {
-public function index($name)
-{
-    return "This is TestController - " . $name;
-}
+    public function index($name)
+    {
+        return "This is TestController - " . $name;
+    }
 }
